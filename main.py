bf0338624b37b260973135b18ef4046f391189a3
summerWinter63 <lalit.wadhvana@gmail.com>
2021-08-31 21:24:23 -0400

    added name and address details and printed

--- main.py
+++ main.py
@@ -9,8 +9,20 @@ def print_hi(name, age):
     print(f'Hi, {name} your age is {int(age)}')  # Press Ctrl+F8 to toggle the breakpoint.
 
 
+def mystat():
+    fname = input('your first name: ')
+    lname = input('your last name:  ')
+    snumber = input('street number: ')
+    sname = input('street name: ')
+    city = input('city: ')
+    print(f"This is me: {fname} {lname}, {snumber} {city} ")
+    return fname, lname, snumber, sname, city
+
+
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('Lalit W', 50)
+    fname, lname, snumber, sname, city = mystat()
+
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
