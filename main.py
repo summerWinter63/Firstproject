0b12b522778d40cd3830049b997591ef72ab502a
summerWinter63 <lalit.wadhvana@gmail.com>
2021-08-31 21:02:08 -0400

    Fix text to match intent

--- main.py
+++ main.py
@@ -6,11 +6,11 @@
 
 def print_hi(name, age):
     # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name} age is {age}')  # Press Ctrl+F8 to toggle the breakpoint.
+    print(f'Hi, {name} your age is {int(age)}')  # Press Ctrl+F8 to toggle the breakpoint.
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('Lalit PyCharm','erfefqe')
+    print_hi('Lalit W', 50)
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
